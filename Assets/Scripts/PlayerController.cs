c533d72e7e11a90bc1ce7da5864e1c655a130231
kalkazar13 <joecurrystodder@gmail.com>
2022-03-24 16:02:05 -0400

    Fixed shooting; added enemy placeholder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ProjectileScript; // consider using a "Namespace", whatever that is
 
 public class PlayerController : MonoBehaviour // TODO: consider renaming
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
     public SpriteRenderer spriteRenderer;
+    public GameObject bulletPrefab; // TODO: consider moving elsewhere
+
+    [SerializeField] private LayerMask jumpableGround; // LEARN WHAT THIS IS LATER
     
     // consider renaming the following. Maybe remove the "anim" from them
     // also consider making them something other than sprites
@@ -20,32 +26,60 @@ public class PlayerController : MonoBehaviour // TODO: consider renaming
 
     // CREATE MORE SPRITES FOR CROUCHING AND JUMPING
 
-    private bool crouching = false; // is this necessary? Consider deleting
+    private float runSpeed = 3.0f; // TODO: consider changing
+    private float crouchSpeed = 1.0f; // TODO: consider changing. Maybe make slower
+    private bool crouching = false;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Move Horizontal"); // TODO: make movement speed slower when crouching
+        Move();
+        // TODO: consider adding the following to a new method. Like "FireRifle()"
+        float shootX = Input.GetAxisRaw("Fire Horizontal"); // TODO: consider not using axes for this
+        float shootY = Input.GetAxisRaw("Fire Vertical"); // TODO: consider not using axes for this
+        // TODO: ADD MORE STUFF LATER
+        if (shootX != 0 || shootY != 0) {
+            Debug.Log("shooting called: xDir=" + shootX + ", yDir=" + shootY); // TODO: delete later
+            FireBullet(shootX, shootY);
+        }
+    }
+
+    private bool IsGrounded() { // TODO: consider moving down
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    private void Move() { // consider renaming "Movement()"
+        float moveX = Input.GetAxisRaw("Move Horizontal");
         float moveY = Input.GetAxisRaw("Move Vertical");
-        // Debug.Log(moveY);
-        rb.velocity = new Vector2(moveX * 3.0f, rb.velocity.y); // TODO: consider changing speed
-        crouching = moveY < 0; // consider turning into a ternary
-        if (crouching) {
-            spriteRenderer.sprite = idleCrouchingAnim; // consider simplifying. Also move it down
-        } else {
-            spriteRenderer.sprite = idleAnim;
-        }
-        // if (Input.GetKeyDown("space") || moveY > 0) {
-        if (Input.GetButtonDown("Jump")) {
+        crouching = moveY < 0 && IsGrounded(); // TODO: maybe make it so you can flip in the air
+        rb.velocity = new Vector2(moveX * (crouching ? crouchSpeed : runSpeed), rb.velocity.y);
+        spriteRenderer.sprite = crouching ? idleCrouchingAnim : idleAnim;
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
             rb.velocity = new Vector2(rb.velocity.x, 7f);
-            // Make it so you can't jump in the air. Also control jump height by holding down button
+            // TODO: make it so you can control jump height by holding down the jump button
+            // ALSO TODO: consider changing fall speed while firing
         }
+    }
+
+    // TODO: consider changing fall speed while firing
+    private void FireBullet(float xDir, float yDir) { // TODO: consider renaming. Maybe "FireRifle"
+        GameObject obj = Instantiate(bulletPrefab) as GameObject;
+        float aimHeight = transform.position.y;
+        if (crouching) {
+            aimHeight -= (coll.bounds.size.y / 4);
+        } else {
+            aimHeight += (coll.bounds.size.y / 4);
+        }
+        obj.transform.position = new Vector2(transform.position.x, aimHeight); 
+        ProjectileScript projectile = obj.GetComponent<ProjectileScript>();
+        projectile.SetDirection(xDir, yDir);
     }
 }
--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileScript : MonoBehaviour
+public class ProjectileScript : MonoBehaviour // TODO: consider renaming to just "Projectile"
 {
     private Vector2 screenBounds;
     private Rigidbody2D rb;
     public float xDir = 0f; // is this right? consider later
     public float yDir = 0f; // is this right? consider later
+    public float speed = 7f; // consider making private. Also consider changing speed
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,14 @@ public class ProjectileScript : MonoBehaviour
             new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)
         );
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(xDir, yDir);
+        // TODO: should probably do the rest of the "SetDirection" stuff here
+        rb.velocity = new Vector2(xDir * speed, yDir * speed); // TODO: consider moving elsewhere, or deleting
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(OutOfBounds()) { // add something for when a bullet hits a wall
+        if (OutOfBounds()) { // TODO: add something for when a bullet hits a wall or an enemy
             Destroy(this.gameObject);
         }
     }
@@ -34,7 +36,14 @@ public class ProjectileScript : MonoBehaviour
             || transform.position.y > screenBounds.y * 2;
     }
 
-    // public void SetDirection(float xDir, float yDir) { // TODO: CONSIDER USING C# SHORTCUTS
-    //     //
-    // }
+    public void SetDirection(float xDirNew, float yDirNew) {
+        xDir = xDirNew;
+        yDir = yDirNew;
+        float rotation = Mathf.Rad2Deg * Mathf.Atan(yDir/xDir);
+        if (xDir < 0) {
+            rotation += 180;
+        }
+        transform.localRotation = Quaternion.Euler(0, 0, rotation);
+        rb.velocity = new Vector2(xDir * speed, yDir * speed);
+    }
 }
